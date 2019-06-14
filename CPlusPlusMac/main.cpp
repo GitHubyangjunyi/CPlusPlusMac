d579204feb5230d13376e0bff6cc45e3ce267054
杨俊艺 <1209032479@qq.com>
2019-06-14 16:52:21 +0800

    Initial Commit

---
+++ CPlusPlusMac/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPlusPlusMac
+//
+//  Created by 杨俊艺 on 2019/6/14.
+//  Copyright © 2019 杨俊艺. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
